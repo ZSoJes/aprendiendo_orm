4502558f2571ff3a2d241e08da6e1ce22a0d87ac
ZSoJes <gomezsotelojuan@gmail.com>
2016-10-02 23:09:09 -0500

    realice pruebas en chef y algunas salieron

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require_relative 'app'
 
 def assert(truthy)
@@ -7,8 +8,36 @@ end
 chef = Chef.find(1)
 
 # Este es un ejemplo de test ya que los nombres de los chefs son aleatorios, este test muy probablemente fallará
-assert chef[:first_name] == 'Ferran'
-assert chef[:last_name] == 'Adria'
+assert chef != nil
+assert chef[:first_name] == 'Eduardo'
+assert chef[:last_name] == 'Fernández' 
+# p chef[:first_name] = 'Eduard'	si sirve
+# assert chef[:first_name] == "Eduard"	si sirve
+
+#chef.create {"Juan","Gomez","1994-11-27","10orangeblue@gmail.com","48393843"}
 
 
 puts "finished"
+# Nota: El programa lanzara el error de ejecucion 
+# si alguna prueba se lleva mal
+
+
+# metodos de chef
+# self.all         				check
+# self.create(attributes)  		no check
+# self.where(query, *args)		no check
+# self.find(pk)					check
+# save							funciona junto con self.create
+# new_record?					funciona junto con new record
+
+# e.g., chef[:first_name] #=> 'Steve'
+# [](attribute)					check
+
+# e.g., chef[:first_name] = 'Steve'
+# []=(attribute, value)			check
+
+# meals
+# add_meals(meals)
+
+# insert!
+# update!
